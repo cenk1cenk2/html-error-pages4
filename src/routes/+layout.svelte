cfc95f8e3121782c7cfcefa60cb956f39f058cfa
Cenk Kılıç <cenk@kilic.dev>
2023-04-30 21:24:49 +0200

    fix: update configuration for dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,19 +3,10 @@
   import '../app.css'
 </script>
 
-<svelte:head>
-  <script>
-    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-  </script>
-</svelte:head>
-
 <div class="flex overflow-hidden flex-col w-screen h-screen min-h-full lg:justify-center app">
   <main>
     <slot />
   </main>
+
   <Footer />
 </div>
